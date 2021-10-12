1b308ebbc16379fc019efc41d1ae9d8bd042b20d
Harrison Gerdes <harrisongerdes@gmail.com>
2021-10-12 14:05:27 -0400

    fixed getInstruments code

--- frontend/src/components/InstrumentPage/index.js
+++ frontend/src/components/InstrumentPage/index.js
@@ -10,27 +10,31 @@ const InstrumentsPage = () => {
     const dispatch = useDispatch();
     const { instrumentId } = useParams();
 
+
     useEffect(() => {
         dispatch(getInstruments())
     }, [dispatch])
 
     const instruments = useSelector(state => {
-        return state.instruments.list;
+    console.log("state.instruments value= ", state.instruments)
+
+        return state.instruments.getAllInstruments;
         //  return state.instruments[instrumentId]
         // return state.instruments.list.map(instrumentId => state.instruments[instrumentId])
     })
 
+
     return (
         <div className="instruments-container">
-            {instruments?.map((instrument) => {
-                return(
-                <div key={instrument.name}>
-                <NavLink className="instruments" to={`/instrument/${instrument.id}`}>
+            {instruments?.map(instrument => (
+                <div key={instrument.id}>
+                <NavLink to={`/instruments/${instrument.id}`} key={`${instrument.id}`} className="instruments">
                     <div className="instrumentName">Instrument: {instrument.name}</div>
                 </NavLink>
+                <p>445</p>
                 </div>
-                );
-            })}
+            ))}
+
         </div>
         )
 }
